56b39ed083b462294b0d31949642f938b7d516a9
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-26 13:05:55 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This resource installs a package called flask
 
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
