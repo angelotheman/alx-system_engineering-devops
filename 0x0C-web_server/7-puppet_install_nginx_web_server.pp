e747c1de07baf3be66b0addea6a379771808433b
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-28 09:53:35 +0000

    Using puppet to accomplish similar

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Using puppet to manage my nginx configurations
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Configure the Nginx Server
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "
+    server {
+	    listen 80;
+		listen [::]:80;
+		
+		server_name 54.237.9.122;
+
+		root /var/www/html;
+		index index.html index.htm index.nginx-debian.html
+
+		location / {
+			return 200 'Hello World!';
+		}
+
+		location /redirect_me {
+			return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
+		}
+	}
+  ",
+}
+
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Ensure Nginx server is always running or restart
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
