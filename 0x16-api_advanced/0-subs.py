4ab0b26a730d1308e9eb78b4fa59ac09898a49ae
Kwabena Twumasi <ktwum721@gmail.com>
2024-04-11 05:50:56 +0000

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,16 +11,9 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    elif response.status_code == 404:
+    if response.status_code == 404:
         return 0
-    else:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
